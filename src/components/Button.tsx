cc4b9a2d7504d677f70999ffdec1081c6197b57f
Dale B <daleban.dev@gmail.com>
2021-08-28 17:24:27 +0800

    Initializse button design.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+
 import React, { useState } from "react";
 
 interface ButtonProps {
@@ -14,7 +17,31 @@ export const Button: React.FC<ButtonProps> = ({
   const [showBtn, setShowBtn] = useState<boolean>(true);
 
   return (
-    <div>
+    <div
+      className="Button-center"
+      css={css`
+        button {
+          color: #e0e0e0;
+          height: 100px;
+          width: 400px;
+          outline: none;
+          border: #e0e0e0 3px solid;
+          border-radius: 50px;
+          font-size: 2rem;
+          font-weight: 600;
+          position: relative;
+          top: 150px;
+          background: #16202c;
+          transition: linear 0.3s;
+          cursor: pointer;
+
+          &:hover {
+            background: #1f2d3e;
+            font-size: 2.01rem;
+          }
+        }
+      `}
+    >
       {showBtn && (
         <button
           onClick={() => {
@@ -23,7 +50,7 @@ export const Button: React.FC<ButtonProps> = ({
             getIpData();
           }}
         >
-          GET IP Details
+          GET IP DETAILS
         </button>
       )}
     </div>
