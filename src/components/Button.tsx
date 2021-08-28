235c3d399ea83f55b0198f88b44f755e71e2d986
Dale B <daleban.dev@gmail.com>
2021-08-28 18:11:24 +0800

    Add 3d render hand on button.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
 /** @jsx jsx */
+/** @jsxFrag */
 import { css, jsx } from "@emotion/react";
 
 import React, { useState } from "react";
+import hand from "../img/3dhand.png";
 
 interface ButtonProps {
   getIpData: () => void;
@@ -15,11 +17,26 @@ export const Button: React.FC<ButtonProps> = ({
   loading,
 }) => {
   const [showBtn, setShowBtn] = useState<boolean>(true);
+  const [onHover, setOnHover] = useState<boolean>(false);
 
   return (
     <div
       className="Button-center"
       css={css`
+        .grid-button {
+          display: grid;
+          grid-template-columns: 1fr;
+          place-items: center;
+        }
+
+        .hand {
+          height: 400px;
+          position: relative;
+          top: 40px;
+          transform: rotate(60deg);
+          cursor: pointer;
+        }
+
         button {
           color: #e0e0e0;
           height: 100px;
@@ -31,7 +48,7 @@ export const Button: React.FC<ButtonProps> = ({
           font-weight: 600;
           position: relative;
           top: 150px;
-          background: #16202c;
+          background: transparent;
           transition: linear 0.3s;
           cursor: pointer;
 
@@ -40,18 +57,37 @@ export const Button: React.FC<ButtonProps> = ({
             font-size: 2.01rem;
           }
         }
+        .btn-hover {
+          background: #1f2d3e;
+          font-size: 2.01rem;
+        }
       `}
     >
       {showBtn && (
-        <button
-          onClick={() => {
-            setShowBtn(false);
-            setLoader();
-            getIpData();
-          }}
-        >
-          GET IP DETAILS
-        </button>
+        <div className="grid-button">
+          <button
+            className={onHover ? "btn-hover" : ""}
+            onClick={() => {
+              setShowBtn(false);
+              setLoader();
+              getIpData();
+            }}
+          >
+            GET IP DETAILS
+          </button>
+          <img
+            className="hand"
+            src={hand}
+            alt="3d hand"
+            onMouseEnter={() => setOnHover(true)}
+            onMouseLeave={() => setOnHover(false)}
+            onClick={() => {
+              setShowBtn(false);
+              setLoader();
+              getIpData();
+            }}
+          />
+        </div>
       )}
     </div>
   );
