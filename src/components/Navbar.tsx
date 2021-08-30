eaef17092047d04e7d962b694c878e5ac63c034f
Dale B <daleban.dev@gmail.com>
2021-08-30 13:43:21 +0800

    Add github repo link on navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { AiOutlineInstagram, AiOutlineGithub } from "react-icons/ai";
 import { RiLinkedinFill } from "react-icons/ri";
 import { BsArrowRightShort } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 interface NavbarProps {}
 
@@ -23,6 +24,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
 
         h1 {
           font-weight: 600;
+          cursor: default;
         }
 
         .connect {
@@ -32,26 +34,34 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
             font-size: 1rem;
           }
 
-          &-btn {
+          &-repo-btn {
             display: flex;
             align-items: center;
             margin-left: 1rem;
-            padding: 0 5px 0 10px;
+            padding: 3px 5px 3px 10px;
             border: 1px #e6ebf2 solid;
             border-radius: 25px;
+            cursor: pointer;
+            transition: 0.1s linear;
+
+            &:hover {
+              background: #1f2d3e;
+            }
 
             p {
-              font-size: 1.2rem;
+              font-size: 1rem;
             }
 
             .icon-arrow {
-              font-size: 1.6rem;
+              font-size: 1.5rem;
             }
           }
         }
 
-        a {
-          color: #fff;
+        .connect-icons a {
+          color: #e6ebf2;
+          position: relative;
+          top: 3px;
 
           &:not(:last-child) {
             margin-right: 1rem;
@@ -61,15 +71,21 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
             padding: 5px;
             border: 1px #e6ebf2 solid;
             border-radius: 50%;
+            transition: 0.1s linear;
 
             &:hover {
               background: #1f2d3e;
             }
           }
         }
+
+        .repo-link {
+          text-decoration: none;
+          color: #e6ebf2;
+        }
       `}
     >
-      <h1>eipify.</h1>
+      <motion.h1 whileHover={{ rotate: -20 }}>eipify.</motion.h1>
       <div className="connect">
         <div className="connect-icons">
           <a href="https://www.instagram.com/dale.ps/" target="_blank">
@@ -82,10 +98,16 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
             <RiLinkedinFill className="connect-icons-icon" />
           </a>
         </div>
-        {/* <div className="connect-btn">
-          <p>connect</p>
-          <BsArrowRightShort className="icon-arrow" />
-        </div> */}
+        <a
+          href="https://github.com/hyamero/Eipify-ts"
+          target="_blank"
+          className="repo-link"
+        >
+          <div className="connect-repo-btn">
+            <p>visit repo</p>
+            <BsArrowRightShort className="icon-arrow" />
+          </div>
+        </a>
       </div>
     </nav>
   );
